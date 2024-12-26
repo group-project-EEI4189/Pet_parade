4d75a0a316964271d418fe1930b5d55ffd731ed7
Zahrath Sahna Jabir <116071606+zahrathSjabir@users.noreply.github.com>
2024-12-26 18:47:33 +0530

    Update delete.blade.php

--- resources/views/pets/delete.blade.php
+++ resources/views/pets/delete.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
-
+@section('title', 'Delete Pet')
 @section('content')
 <div class="container">
-    <h1>Confirm Delete</h1>
+    <h1>Delete Pet</h1>
     <p>Are you sure you want to delete {{ $pet->breed }}?</p>
-
-    <form action="{{ route('pets.destroy', $pet->id) }}" method="POST">
+    <form method="POST" action="{{ route('pets.destroy', $pet->id) }}">
         @csrf
         @method('DELETE')
         <button type="submit">Yes, Delete</button>
