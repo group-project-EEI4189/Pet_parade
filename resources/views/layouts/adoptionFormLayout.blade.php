6163e8c51e49eadc164c7ed46072d114653424fd
Zahrath Sahna Jabir <116071606+zahrathSjabir@users.noreply.github.com>
2025-03-05 10:34:36 +0530

    Add files via upload

---
+++ resources/views/layouts/adoptionFormLayout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adopt a Pet</title>
+    <link rel="stylesheet" href="{{ asset('css/adoption.css') }}">
+</head>
+<body>
+    @yield('content')
+
+    <script src="{{ asset('js/adoption.js') }}"></script>
+</body>
+</html>
