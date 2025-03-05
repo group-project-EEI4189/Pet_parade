137a97ce1c8396418279abc7bc1371f0556d4c64
Zahrath Sahna Jabir <116071606+zahrathSjabir@users.noreply.github.com>
2025-03-05 10:37:38 +0530

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Parade</title>
+    <link rel="stylesheet" type="text/css" href="http://127.0.0.1:8000/css/home.css">
+</head>
+<body>
+    <div class="home-up">
+        <header class="home-header">
+            <div class="head">
+                <div class="logo-brand">
+                    <div class="logo"><img src="http://127.0.0.1:8000/HomePageImages/Logo.png" alt="Logo"></div>
+                    <div class="brand">Pet Parade</div>
+                </div>
+                <nav>
+                    <div class="main-nav">
+                    <ul>
+                        <li><a href="http://127.0.0.1:8000">Home</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                    </div>
+                    <div class="auth-nav-btn">
+                        <ul>
+                            <li><a href="" class="login-btn">Login</a></li>
+                            <li><a href="" class="signup-btn">Sign Up</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <main class="home-main">
+            <section class="hero-1">
+                <div class="first-main">
+                    <div class="first-main-content-left">
+                        <img src="http://127.0.0.1:8000/HomePageImages/FamilyWithPet.png" alt="Family with pet">
+                    </div>
+                    <div class="first-main-content-right">
+                        <h1>Swipe. <span class="word-shop">Shop</span>. Snuggle.</h1>
+                        <a href="" class="get-started-btn">Get Started</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>   
+    <div class="home-down">       
+        <div class="third-main">
+            <h1>Ready to Pamper Your Pets?</h1>
+            <p>Discover top-quality pet foods, stylish accessories, fun toys and medicines at</p>
+            <p>Pet Parade to keep your furry friends happy and healthy.</p>
+        </div>
+        <div class="tail">
+            <a href="">Terms &amp; Conditions</a>
+            <a href="">Privacy Policy</a>
+        </div>
+    </div>
+</body>
+</html>
